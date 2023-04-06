a8c0769ddf818590b0c5e48e8d72e254877a987d
Damlrca <sir.damirca@yandex.ru>
2023-04-06 22:20:04 +0300

    basic classes

--- JSON_Lib/JSON_lib.cpp
+++ JSON_Lib/JSON_lib.cpp
@@ -1,6 +1,146 @@
 #include "JSON_lib.h"
 
 namespace JSON_Lib {
+	// Link
+
+	Link::Link(const Link& l) {
+		switch (l.val->get_type())
+		{
+		case ValueType::Value:
+			val = new Value(l.val);
+			break;
+		case ValueType::ListValue:
+			val = new ListValue(l.val);
+			break;
+		default:
+			val = nullptr;
+			break;
+		}
+		if (l.nxt) {
+			nxt = new Link(*l.nxt);
+		}
+		else {
+			nxt = nullptr;
+		}
+		key = l.key;
+	}
+	
+	Link& Link::operator=(const Link& l) {
+		if (this == &l)
+			return *this;
+		delete val;
+		val = nullptr;
+		switch (l.val->get_type())
+		{
+		case ValueType::Value:
+			val = new Value(l.val);
+			break;
+		case ValueType::ListValue:
+			val = new ListValue(l.val);
+			break;
+		default:
+			val = nullptr;
+			break;
+		}
+		delete nxt;
+		nxt = nullptr;
+		if (l.nxt) {
+			nxt = new Link(*l.nxt);
+		}
+		key = l.key;
+		return *this;
+	}
+
+	// ListValue
+
+	ListValue::ListValue(const ListValue& v) {
+		if (v.start) {
+			start = new Link(*v.start);
+			last = start;
+			while (last->nxt)
+				last = last->nxt;
+		}
+		else {
+			start = last = nullptr;
+		}
+	}
+
+	ListValue::ListValue(IValue* iv) {
+		if (iv->get_type() != ValueType::ListValue)
+			throw "ListValue(IValue*): IValue type isn't ListValue";
+		if (((ListValue*)iv)->start) {
+			start = new Link(*(((ListValue*)iv)->start));
+			last = start;
+			while (last->nxt)
+				last = last->nxt;
+		}
+		else {
+			start = last = nullptr;
+		}
+	}
+
+	void ListValue::add(const std::string& key, IValue* val) {
+		if (start) {
+			last->nxt = new Link(key, val);
+			last = last->nxt;
+		}
+		else {
+			last = start = new Link(key, val);
+		}
+	}
+
+	std::string read_key(std::istream& in) {
+		std::string res;
+		char c;
+		if (!(in >> c) || c != '\"')
+			throw "read_key: '\"' expected";
+		while ((in >> c) && c != '\"')
+			res += c;
+		if (c != '\"')
+			throw "read_key: '\"' expected";
+		return res;
+	}
+
+	IValue* read_IValue(std::istream& in) {
+		char first_symbol;
+		if (!(in >> first_symbol))
+			throw "read_IValue: nothing to read";
+		if (first_symbol == '{') {
+			// ListValue
+			ListValue* res = new ListValue{};
+			char c;
+			int cnt = 0;
+			do {
+				if (!(in >> c))
+					throw "read_IValue: eof";
+				if (c == ',' && cnt == 0)
+					throw "read_IValue: '\"' expected";
+				if (c == ',' && !(in >> c))
+					throw "read_IValue: '\"' expected";
+				if (c != '\"')
+					throw "read_IValue: '\"' expected";
+				in.unget();
+				std::string key = read_key(in);
+				if (!(in >> c) || c != ':')
+					throw "read_IValue: ':' expected";
+				IValue* iv = read_IValue(in);
+				res->add(key, iv);
+				cnt++;
+			} while (c != '}');
+			return res;
+		}
+		else if (first_symbol == '\"') {
+			// Value
+			in.unget();
+			std::string t = read_key(in);
+			Value* v = new Value(t);
+			return v;
+		}
+		else {
+			throw "read_IValue: wrong first symbol";
+		}
+	}
+
 	int JSON::test() {
 		return 1234;
 	}
--- JSON_Lib/JSON_lib.h
+++ JSON_Lib/JSON_lib.h
@@ -1,7 +1,77 @@
 #ifndef _JSON_Lib
 #define _JSON_Lib
 
+#include <string>
+#include <iostream>
+
 namespace JSON_Lib {
+	enum class ValueType {
+		Value,
+		ListValue
+	};
+
+	class IValue {
+	public:
+		virtual ValueType get_type() = 0;
+		virtual std::string get_val() = 0;
+	};
+
+	std::string read_key(std::istream& in);
+
+	IValue* read_IValue(std::istream& in);
+
+	class Value : public IValue {
+	private:
+		std::string value;
+	public:
+		Value() = default;
+		Value(const Value&) = default;
+		Value(const std::string& _val) : value(_val) {}
+		Value(IValue* iv) {
+			if (iv->get_type() != ValueType::Value)
+				throw "Value(IValue*): IValue type isn't Value";
+			value = iv->get_val();
+		}
+		ValueType get_type() { return ValueType::Value; }
+		std::string get_val() { return value; }
+		Value& operator=(const Value&) = default;
+		~Value() = default;
+	};
+
+	struct Link {
+		std::string key;
+		IValue* val;
+		Link* nxt;
+		Link(const std::string& _key, IValue* _val, Link* _nxt = nullptr)
+			: key(_key), val(_val), nxt(_nxt) {}
+		Link(const Link& l);
+		Link& operator=(const Link& l);
+		~Link() {
+			delete nxt;
+			delete val;
+		}
+	};
+
+	class ListValue : public IValue {
+	private:
+		Link* start;
+		Link* last;
+	public:
+		ValueType get_type() { return ValueType::ListValue; }
+		std::string get_val() { throw "ListValue doesn't have value"; }
+		ListValue() : start(nullptr), last(nullptr) {}
+		ListValue(const ListValue& v);
+		ListValue(IValue* iv);
+		void add(const std::string& key, IValue* val);
+		void clear() {
+			delete start;
+			start = last = nullptr;
+		}
+		~ListValue() {
+			clear();
+		}
+	};
+
 	class JSON {
 	public:
 		static int test();
